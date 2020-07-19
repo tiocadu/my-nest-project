15525eac5fc7f1c35d27223d6f26923168cf433b
Carlos Eduardo <edugaspar@hotmail.com>
2020-07-19 17:42:59 -0300

    feat: adds error handling in get by id method

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { v4 as uuidv4 } from 'uuid';
 import { CreateTaskDto } from './dto/create-task.dto';
@@ -51,7 +51,13 @@ export class TasksService {
   }
 
   getTaskById(id: string): Task {
-    return this.tasks.find(task => task.id === id);
+    const foundTask = this.tasks.find(task => task.id === id);
+
+    if (!foundTask) {
+      throw new NotFoundException(`Task with ID ${id} not found`);
+    }
+
+    return foundTask;
   }
 
   createTask(createTaskDto: CreateTaskDto): Task {
