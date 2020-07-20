9da21fde852aaac4d6792131da7746c9a4923e34
Carlos Eduardo <edugaspar@hotmail.com>
2020-07-19 23:53:01 -0300

    style: prettier changes

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,12 +1,12 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
-    type: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    username: 'postgres',
-    password: '',
-    database: 'taskmanagement',
-    entities: [__dirname + '/../**/*.entity.{js,ts}'],
-    synchronize: true, // not recomended in production
-}
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: '',
+  database: 'taskmanagement',
+  entities: [__dirname + '/../**/*.entity.{js,ts}'],
+  synchronize: true, // not recomended in production
+};
