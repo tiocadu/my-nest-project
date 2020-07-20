49e97b8cbf9603827db3eb05f58229f58b1769a9
Carlos Eduardo <edugaspar@hotmail.com>
2020-07-19 23:52:05 -0300

    test: fix test including imports but THIS DOESNT MAKE SENSE!
    I will have to understand how to better test this.

--- src/tasks/tasks.controller.spec.ts
+++ src/tasks/tasks.controller.spec.ts
@@ -1,12 +1,17 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TasksController } from './tasks.controller';
 import { TasksService } from './tasks.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TaskRepository } from './task.repository';
+import { TasksModule } from './tasks.module';
+import { typeOrmConfig } from '../config/typeorm.config';
 
 describe('Tasks Controller', () => {
   let controller: TasksController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [TypeOrmModule.forRoot(typeOrmConfig), TasksModule, TypeOrmModule.forFeature([TaskRepository])],
       controllers: [TasksController],
       providers: [TasksService],
     }).compile();
--- src/tasks/tasks.service.spec.ts
+++ src/tasks/tasks.service.spec.ts
@@ -1,11 +1,16 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TasksService } from './tasks.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmConfig } from '../config/typeorm.config';
+import { TasksModule } from './tasks.module';
+import { TaskRepository } from './task.repository';
 
 describe('TasksService', () => {
   let service: TasksService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [TypeOrmModule.forRoot(typeOrmConfig), TasksModule, TypeOrmModule.forFeature([TaskRepository])],
       providers: [TasksService],
     }).compile();
 
