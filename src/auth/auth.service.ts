1076e1b4722a9a696c964c450b7d611631b8c60d
Carlos Eduardo <edugaspar@hotmail.com>
2020-08-02 20:19:36 -0300

    feat: validation to auth credentials dto and format

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -5,12 +5,12 @@ import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 
 @Injectable()
 export class AuthService {
-    constructor(
-        @InjectRepository(UserRepository)
-        private userRepository: UserRepository,
-    ) {};
+  constructor(
+    @InjectRepository(UserRepository)
+    private userRepository: UserRepository,
+  ) {}
 
-    async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
-        return this.userRepository.signUp(authCredentialsDto);
-    }
+  async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    return this.userRepository.signUp(authCredentialsDto);
+  }
 }
--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -4,12 +4,12 @@ import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
-    async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
-        const { username, password } = authCredentialsDto;
+  async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentialsDto;
 
-        const user = new User();
-        user.username = username;
-        user.password = password;
-        await user.save();
-    }
+    const user = new User();
+    user.username = username;
+    user.password = password;
+    await user.save();
+  }
 }
